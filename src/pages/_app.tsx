1622aeda82b545e43564f58332e6a1d7bd7daaa5
Younes Lahouiti <loller763@gmail.com>
2023-02-27 11:42:26 +0100

    Ajout de transitions de page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,6 +9,7 @@ import "../sass/main.scss";
 //Components
 import PageLayout from "../components/PageLayout/PageLayout";
 
+import { useRouter } from "next/router";
 /**
  * Root component where all the pages will pass through
  *
@@ -20,14 +21,48 @@ import PageLayout from "../components/PageLayout/PageLayout";
  *
  * This file **must not** be nested inside a React Fragment
  */
+
+/*
+  
+*/
+import { motion } from "framer-motion";
+
 export default function App({ Component, pageProps }: AppProps): JSX.Element {
+  const router = useRouter();
+
   return (
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
       <PageLayout>
-        <Component {...pageProps} />
+        <motion.div
+          key={router.route}
+          initial="pageInitial"
+          animate="pageAnimate"
+          exit="pageExit"
+          variants={{
+            pageInitial: {
+              opacity: 0,
+              x: "-100vw",
+            },
+            pageAnimate: {
+              opacity: 1,
+              x: 0,
+            },
+            pageExit: {
+              opacity: 0,
+              x: "100vw",
+            },
+          }}
+          transition={{
+            type: "tween",
+            ease: "anticipate",
+            duration: 0.5,
+          }}
+        >
+          <Component {...pageProps} />
+        </motion.div>
       </PageLayout>
     </>
   );
