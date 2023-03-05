6caec5055d0e799f76718cadf8d1809fd809cb13
Younes Lahouiti <loller763@gmail.com>
2023-03-05 15:11:18 +0100

    Débug UI

--- src/components/Timeline/Timeline.tsx
+++ src/components/Timeline/Timeline.tsx
@@ -33,7 +33,7 @@ export default function Timeline({
                   <span>
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 24 24"
+                      viewBox="0 0 22 22"
                       width={22}
                       height={22}
                     >
