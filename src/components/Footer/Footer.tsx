54ea8ba8e150d9cc9410babae64ac4add023d419
Younes Lahouiti <loller763@gmail.com>
2023-03-06 14:48:01 +0100

    Ajout du lien vers le footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,4 +1,8 @@
+//React
 import React from "react";
+
+//Link
+import Link from "next/link";
 
 export default function Footer(): JSX.Element {
   const currentYear = new Date().getFullYear();
@@ -8,6 +12,14 @@ export default function Footer(): JSX.Element {
       <small className="footer__copyright">
         Younes Lahouiti © 2021-{currentYear}. All rights reserved.
       </small>
+
+      <Link
+        href="https://github.com/LePhenix47/Dev-Portfolio_Younes-Lahouiti"
+        className="footer__link"
+        target="_blank"
+      >
+        View the source code for the portfolio?
+      </Link>
     </footer>
   );
 }
