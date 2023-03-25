e8b5a637541208018953295578f6c57ffcb2d2e0
Younes Lahouiti <loller763@gmail.com>
2023-03-25 17:18:27 +0100

    Addition of the effect on select

--- src/react-utils/variables/about-cards.variables.tsx
+++ src/react-utils/variables/about-cards.variables.tsx
@@ -2,19 +2,27 @@
 import { aboutCardsTypes } from "../types/about-cards.types";
 
 /**
- * The current year as a number.
+ * Calculates the amount of experience in years based on the current year and the year when my career started.
+ * @returns {number} The amount of experience in years.
  */
-const currentDate: number = new Date().getFullYear();
+function getAmountOfExperience(): number {
+  /**
+   * The current year as a number.
+   */
+  const currentDate: number = new Date().getFullYear();
 
-/**
- * The year when the career started as a number.
- */
-const careerDateStart: number = new Date("06/28/2022").getFullYear();
+  /**
+   * The year when the career started as a number.
+   */
+  const careerDateStart: number = new Date("06/28/2022").getFullYear();
+
+  return currentDate - careerDateStart;
+}
 
 /**
  * The amount of experience in years, calculated by subtracting the `careerDateStart` from the `currentDate`.
  */
-const amountOfExperience: number = currentDate - careerDateStart;
+const amountOfExperience: number = getAmountOfExperience();
 
 /**
  * Variables for the about cards
