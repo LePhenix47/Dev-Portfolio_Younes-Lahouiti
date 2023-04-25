3788c5e9dfe67ce086daa6efeb866034dc09083e
Younes Lahouiti <loller763@gmail.com>
2023-04-25 17:52:57 +0200

    Aiddiotion of 2/3 diplomas in the public folder

---
+++ src/react-utils/variables/diplomas.variables.ts
@@ -0,0 +1,24 @@
+/**
+ * Diplomas I earned
+ */
+export const diplomas: {
+  name: string;
+  year: string;
+  pdf: any;
+}[] = [
+  {
+    name: "Baccalureate",
+    year: "2020",
+    pdf: "../../../public/pdf/BACCALAUREAT GENERAL.pdf",
+  },
+  {
+    name: "Level 5 RNCP Title",
+    year: "2022",
+    pdf: "",
+  },
+  {
+    name: "Level 6 RNCP Title",
+    year: "2023",
+    pdf: "../../../public/pdf/diplome OC RNCP niveau 6.pdf",
+  },
+];
