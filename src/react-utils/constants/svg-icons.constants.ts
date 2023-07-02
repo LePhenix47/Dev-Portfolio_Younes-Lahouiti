b00b02feacc4087faec563c7009340a4846e0a31
Younes Lahouiti <loller763@gmail.com>
2023-07-02 16:06:44 +0200

    Addition of icons inside a constant to later refactor the code + fixed TS with type assertions

---
+++ src/react-utils/constants/svg-icons.constants.ts
@@ -0,0 +1,126 @@
+//Common icons
+import bug from "../../../public/svg/bug.svg";
+
+import openNewTab from "../../../public/svg/open-in-new-tab-icon.svg";
+
+import upArrow from "../../../public/svg/arrow.svg";
+
+//Home
+import sendMail from "../../../public/svg/send.svg";
+
+import blob from "../../../public/svg/blob.svg";
+
+//About
+import medal from "../../../public/svg/medal.svg";
+
+import briefcase from "../../../public/svg/briefcase.svg";
+
+import headset from "../../../public/svg/headset.svg";
+
+import resume from "../../../public/svg/files.svg";
+
+//Skills
+import badgeCheck from "../../../public/svg/badge-check.svg";
+
+import calendar from "../../../public/svg/calendar.svg";
+
+//Services
+import integration from "../../../public/svg/integration.svg";
+
+import settings from "../../../public/svg/settings.svg";
+
+import code from "../../../public/svg/code.svg";
+
+import check from "../../../public/svg/check.svg";
+
+//Portfolio
+import search from "../../../public/svg/magnifying-glass.svg";
+
+//Contact
+import email from "../../../public/svg/email.svg";
+
+import slack from "../../../public/svg/slack-logo.svg";
+
+import whatsApp from "../../../public/svg/whatsapp-logo.svg";
+
+//Mobile
+import homeHeader from "../../../public/svg/home-header.svg";
+
+import aboutHeader from "../../../public/svg/about-header.svg";
+
+import skillsHeader from "../../../public/svg/skills-header.svg";
+
+import servicesHeader from "../../../public/svg/briefcase-header.svg";
+
+import portfolioHeader from "../../../public/svg/image-header.svg";
+
+import contactHeader from "../../../public/svg/contact-header.svg";
+
+//Socials icons
+import youtube from "../../../public/svg/youtube-icon.svg";
+
+import twitter from "../../../public/svg/twitter-icon.svg";
+
+import linkedIn from "../../../public/svg/linkedin-icon.svg";
+
+import gitHub from "../../../public/svg/github-icon.svg";
+
+import stackOverflow from "../../../public/svg/stackoverflow-icon.svg";
+
+import npm from "../../../public/svg/npm-icon.svg";
+
+import discord from "../../../public/svg/discord-icon.svg";
+
+import codePen from "../../../public/svg/codepen-icon.svg";
+
+const commonIcons = {};
+
+const homeIcons = {};
+
+const aboutIcons = {};
+
+const skillsIcons = {};
+
+const servicesIcons = {};
+
+const portfolioIcons = {};
+
+const contactIcons = {};
+
+const SVG_ICONS = {
+  openNewTab,
+  bug,
+  upArrow,
+  blob,
+  resume,
+  medal,
+  briefcase,
+  headset,
+  sendMail,
+  badgeCheck,
+  calendar,
+  integration,
+  settings,
+  code,
+  check,
+  search,
+  email,
+  slack,
+  whatsApp,
+  homeHeader,
+  aboutHeader,
+  skillsHeader,
+  servicesHeader,
+  portfolioHeader,
+  contactHeader,
+  youtube,
+  twitter,
+  linkedIn,
+  gitHub,
+  stackOverflow,
+  npm,
+  discord,
+  codePen,
+};
+
+export default SVG_ICONS;
